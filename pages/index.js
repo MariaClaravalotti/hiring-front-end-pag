f3503aab7253cc3d846ef5a55407f86c47106ae9
MariaClaraValotti <email do usurio>
2024-07-03 12:09:23 -0300

    locar

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,26 @@
-
+import { Swiper, SwiperSlide } from 'swiper/react'
+import {register} from 'swiper/element/bundle'
+register();
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import {useState, useEffect} from "react"
+
 
 
 export default function Home() {
+
+  const carros = [
+    {id: "1", img:"civic.png"},
+    {id:"2", img:"hb20.png"},
+    {id:"3",img:"corolla.png"}
+]
+
+
+ 
   return (
     <>
       <Head>
@@ -12,8 +29,48 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="stylesheets" href="./css/style.css/" />
+      
       </Head>
-       
-    </>
-  );
+      
+      <header>
+                <section >
+                    <img src="logocar.png" />
+
+                </section>
+                <section>
+                    <img className="gpsM" src="gps2.png" />
+                    <img className="gpsM" src="linkedin.png" />
+                </section>
+            </header>
+            <main>
+
+                <nav class="pags">
+                    <ul>
+                        <li > <a href="home">HOME</a></li>
+                        <li ><a href="favoritos">FAVORITOS</a></li>
+                        <li  ><a href="reservar">RESERVADOS</a></li>
+                    </ul>
+                </nav>
+
+                <div class="swiper">
+                    <Swiper 
+                       slidesPerView={1}
+                       pagination={{ clickable: true }}
+                       navigation
+                       >
+                    {carros.map( (item) => (
+
+                 <SwiperSlide key={item.id}>
+                   <img  className="slide" src={item.img} alt=""/>
+                 </SwiperSlide>
+
+))}
+            
+                    </Swiper>
+
+                </div>
+                
+            </main>
+        </>
+    );
 }
--- pages/reservar.js
+++ pages/reservar.js
@@ -1,43 +1,37 @@
 
 
-
-
-
-
-
-
-
-export default function Reservado(){
-    return(
-        <body>
+export default function Reservado() {
+    return (
+        <>
             <section class="seçao">
-                <img  class="hb20" src="hb20.png"/>
+                <img class="hb20" src="hb20.png" />
 
-                <figure>Hb20 é uma otima escolha pois possui controle de estabilidade, airbags laterais, cinto de segurança de três pontas e uma estrutura robusta que garante bastante solidez nas curvas em toda a sua gama de modelos</figure>
+                <figure class="fig">Hb20 é uma otima escolha pois possui controle de estabilidade, airbags laterais, cinto de segurança de três pontas e uma estrutura robusta que garante bastante solidez nas curvas em toda a sua gama de modelos</figure>
                 <figcaption>$120/DIA</figcaption>
-                <p>Retirada dia 05/07/2024 ás 12:00</p>
+                <p>Retirada dia 05/07/2024 ás 12:00*</p>
             </section>
 
 
-        <h1>Formulario de Reserva</h1>
-        <forms class="formulario">
-        <label for="name">NOME</label>
-           <input type="text" id="name"/>
+            <h1>Formulario de Reserva</h1>
+            <forms class="formulario">
+                <label for="name">NOME</label>
+                <input type="text" id="name" />
 
-           <label for="email">E-mail</label>
-           <input type="email" id="email"/> 
 
-           <label for="password">SENHA</label>
-           <input type="password" id="password"/>
+                <label for="email">E-mail</label>
+                <input type="email" id="email" />
 
-           <label for="">NAOME</label>
-           <input type="text" id="name"/>
-           
-           <label for="message">Mensagem</label>
-           <textarea name="message" id="message"></textarea>
+                <label for="password">SENHA</label>
+                <input type="password" id="password" />
 
-           <button class="botão" type="submit">Enviar</button>
-        </forms>
-        </body>
+                <label for="number">Telefone</label>
+                <input type="number" id="number" />
+
+                <label for="message">Mensagem</label>
+                <textarea name="message" id="message"></textarea>
+
+                <button href="home.js" class="botão" type="submit">Enviar</button>
+            </forms>
+        </>
     )
 }
